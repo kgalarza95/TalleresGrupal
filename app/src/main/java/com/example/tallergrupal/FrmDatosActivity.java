39911c3b6f23a874c558ae0b7098d5d349947b48
kgalarza <kevin_galata@hotmail.com>
2020-12-19 02:07:07 -0500

    Se agrega la funcionalidad para iniciar el nuevo activity de formulario

--- app/src/main/java/com/example/tallergrupal/FrmDatosActivity.java
+++ app/src/main/java/com/example/tallergrupal/FrmDatosActivity.java
@@ -11,9 +11,23 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class FrmDatosActivity extends AppCompatActivity {
 
+    private TextView txtTitulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frm_datos);
+
+        txtTitulo = findViewById(R.id.txtTitulo);
+        recibirDatosIntent();
+    }
+
+    /**
+     * Me permite obtener el intent y los extras del intetn para administrarlos
+     * la busqueda de los extras es por la clave del valor
+     */
+    private void recibirDatosIntent(){
+        Bundle bundle = getIntent().getExtras();//extraer extras enviadas del activity ant.
+        String nomUsuario = bundle.getString("keyUsuario");
+        txtTitulo.setText("Formulario Datos "+nomUsuario);
     }
 }
