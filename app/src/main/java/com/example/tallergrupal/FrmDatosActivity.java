531af4d1c20f13b5aacd56b40cfe50718da09779
kgalarza <kevin_galata@hotmail.com>
2020-12-18 23:27:54 -0500

    Se agrega la pantalla para el diseño del formulario

---
+++ app/src/main/java/com/example/tallergrupal/FrmDatosActivity.java
@@ -0,0 +1,19 @@
+package com.example.tallergrupal;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ *
+ */
+public class FrmDatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frm_datos);
+    }
+}
